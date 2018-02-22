cf8e7dbfa4d30ce6d2935c891952331b1bd5061b
AsmaaMohamed <smasimo81@gmail.com>
2018-02-22 10:07:33 +0200

    f

--- app/src/main/java/com/example/ok/atmfinder/Call_US.java
+++ app/src/main/java/com/example/ok/atmfinder/Call_US.java
@@ -1,13 +1,116 @@
 package com.example.ok.atmfinder;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class Call_US extends AppCompatActivity {
+import com.example.ok.atmfinder.Activities.About_Us;
+import com.example.ok.atmfinder.Activities.AtmActivity;
+import com.example.ok.atmfinder.Activities.Banks;
+import com.example.ok.atmfinder.Activities.MapsActivity;
+import com.example.ok.atmfinder.Activities.Places;
+
+public class Call_US extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call__us);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.places) {
+
+            Intent i = new Intent(Call_US.this,Places.class);
+            startActivity(i);
+
+
+        } else if (id == R.id.bank) {
+            Intent i = new Intent(Call_US.this,Banks.class);
+            startActivity(i);
+
+        } else if (id == R.id.map) {
+            Intent i = new Intent(Call_US.this,MapsActivity.class);
+            startActivity(i);
+
+        } else if (id == R.id.share) {
+            Intent i = new Intent(Call_US.this,Places.class);
+            startActivity(i);
+
+        } else if (id == R.id.we) {
+            Intent i = new Intent(Call_US.this,About_Us.class);
+            startActivity(i);
+
+        } else if (id == R.id.callus) {
+            Intent i = new Intent(Call_US.this,Call_US.class);
+            startActivity(i);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
